3b5637558a6b56475f1f6a659b9885f8c0dfe8da
Duo <shatz10@outlook.com>
2025-12-05 16:20:06 +0800

    fill red test

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@ int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
     std::cout << "Image created: " << image.get_width() << "x" << image.get_height() << std::endl;
 
-    image.set(52, 41, red);
-    std::cout << "Pixel set at (52, 41)" << std::endl;
+    for (int x = 0; x < 100; x++) {
+        for (int y = 0; y < 100; y++) {
+            image.set(x, y, red);
+        }
+    }
+    
+//    image.set(52, 41, red);
+//    std::cout << "Pixel set at (52, 41)" << std::endl;
 
     image.flip_vertically();
     std::cout << "Image flipped vertically" << std::endl;
